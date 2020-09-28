95eeba23fb7f4616853af48092fadc7b8531ab49
Newcats组-Edwin-姚双贵 <1070342164@qq.com>
2020-09-28 10:21:22 +0800

    add some simple

---
+++ 工作&学习.Net训练营/Domain/Model/TestModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Model
+{
+    /// <summary>
+    /// 轻松使用VS快捷键：Ctrl+Alt 选中多行编辑private
+    /// Ctrl+Shift+V 查看剪切板历史记录
+    /// </summary>
+    public class TestModel
+    {
+        private int Id { get; set; }
+
+        private string Name { get; set; }
+
+        private int Age { get; set; }
+    }
+}
--- 工作&学习.Net训练营/Practice_Web/Controllers/DefaultController.cs
+++ 工作&学习.Net训练营/Practice_Web/Controllers/DefaultController.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PracticeSiteWeb.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly UserServices userServices;
+
+        public DefaultController(UserServices userServices)
+        {
+            this.userServices = userServices;
+        }
+
         public IActionResult Index()
         {
             return View();
