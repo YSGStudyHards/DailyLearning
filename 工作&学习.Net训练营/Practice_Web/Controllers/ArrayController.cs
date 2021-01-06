7327f7ad445d9522d102400b456608cbdc6eaf8a
Newcats组-Edwin-姚双贵 <1070342164@qq.com>
2021-01-06 19:52:16 +0800

    定义多个不同类型的数组

---
+++ 工作&学习.Net训练营/Practice_Web/Controllers/ArrayController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeSiteWeb.Controllers
+{
+    public class ArrayController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index()
+        {
+            //cc
+            var multiTypeArray = new object[] {"小明",'H',2021,"hello world",'!' };
+
+            foreach (var item in multiTypeArray)
+            {
+                var currentValue = item;
+            }
+
+            return View();
+        }
+    }
+}
