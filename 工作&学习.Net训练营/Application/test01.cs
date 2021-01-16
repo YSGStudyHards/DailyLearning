99671712a6e070163fa2ac01ab793bf2378c9e23
Newcats组-Edwin-姚双贵 <1070342164@qq.com>
2021-01-16 19:10:58 +0800

    String类相关方法学习

---
+++ 工作&学习.Net训练营/Application/test01.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+    public class test01
+    {
+        private protected string name { get; set; }
+    }
+}
--- 工作&学习.Net训练营/Practice_Web/Controllers/QueueController.cs
+++ 工作&学习.Net训练营/Practice_Web/Controllers/QueueController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,9 +13,46 @@ namespace PracticeSiteWeb.Controllers
 {
     public class QueueController : Controller
     {
+
+        [HttpPost]
         public IActionResult Index()
         {
-            Queue queue = new System.Collections.Generic.Queue();
+            Queue q = new Queue();
+
+            q.Enqueue("A");
+            q.Enqueue("B");
+            q.Enqueue("C");
+            q.Enqueue("D");
+            q.Enqueue("E");
+
+            foreach (var item in q)
+            {
+                var data = item;
+            }
+
+            q.Enqueue("F");
+            q.Enqueue("G");
+            q.Enqueue("h");
+
+            test02 t = new test02();
+
+            var currentData =t.getName;
+
+            string dataValue = "EFGHIJabcd";
+
+            //string padLeftStr= dataValue.PadLeft(20, 'Z');//.Substring(0,1).ToUpper();
+            //string padRightStr = dataValue.PadLeft(10,'U');
+
+            string s = dataValue.Substring(3);
+            string s2 = dataValue.Substring(0,10);
+
+            string d = "小明同学";
+
+            string str1 = d.Substring(2);
+            string str = d.Substring(2, 1);
+            //string str1 = null;
+            //string str2 = "";
+            //string str3 = String.Empty;
 
             return View();
         }
