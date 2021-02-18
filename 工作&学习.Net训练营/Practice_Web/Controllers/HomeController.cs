990b868cc7d8b75896368cf74535985125e02e8a
yaoshuanggui <1070342164@qq.com>
2021-02-18 22:52:12 +0800

    文件提交

--- 工作&学习.Net训练营/Practice_Web/Controllers/HomeController.cs
+++ 工作&学习.Net训练营/Practice_Web/Controllers/HomeController.cs
@@ -24,22 +24,8 @@ namespace Practice_Web.Controllers
         {
             string classToolJson = "{\"Description\": \"枚举测试\",\"ClassToolDataList\": [{\"ClassTool\": 1}, {\"ClassTool\": 2}, {\"ClassTool\": 3}, {\"ClassTool\": 4},{\"ClassTool\": 5},{\"ClassTool\": 6}, {\"ClassTool\": 7},{\"ClassTool\": 8}, {\"ClassTool\": 9}, {\"ClassTool\":\"\"}]}";
 
-            var dataList = JsonConvert.DeserializeObject<ClassToolModel>(classToolJson);
 
-<<<<<<< HEAD
-            string ImgCode = "ysg20180807/1c5bd382-ae4e-4160-b05b-a58f98509bbe.png";
-
-            //是否存在2018
-            if (ImgCode.IndexOf("2018", StringComparison.Ordinal) != -1)
-            {
-                int indexs= ImgCode.ToString().IndexOf("2018");
-
-                ImgCode = "/uploads/FDImage/Cover/" +
-                          ImgCode.ToString().Substring(ImgCode.ToString().IndexOf("2018"));
-            }
-=======
-            var date = Convert.ToDateTime("00:11:37.770");
->>>>>>> 99671712a6e070163fa2ac01ab793bf2378c9e23
+            var deserializeObject = JsonConvert.DeserializeObject<ClassToolModel>(classToolJson);
 
 
             return View();
