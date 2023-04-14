423721827234909fb4ce12fe4c15ffcc750a3f47
Edwin <1070342164@qq.com>
2023-04-15 01:25:26 +0800

    .NET Core反射获取带有自定义特性的类，通过依赖注入根据Attribute元数据信息调用对应的方法

---
+++ 工作&学习.Net训练营/PracticeConsole/Program.cs
@@ -0,0 +1,105 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+/// <summary>
+/// 自定义一个Attribute类型
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class CustomAttribute : Attribute
+{
+    public string TargetMethod { get; set; }
+
+    public CustomAttribute(string targetMethod)
+    {
+        TargetMethod = targetMethod;
+    }
+}
+
+/// <summary>
+/// 前进服务
+/// </summary>
+[Custom("AdvanceWay")]
+public class AdvanceService
+{
+    public void AdvanceWay()
+    {
+        Console.WriteLine("On the move!");
+    }
+}
+
+/// <summary>
+/// 后退服务
+/// </summary>
+[Custom("RetreatWay")]
+public class RetreatService
+{
+    public void RetreatWay()
+    {
+        Console.WriteLine("Be retreating!");
+    }
+}
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var services = new ServiceCollection();
+
+        //注册需要注入的服务
+        services.AddTransient<AdvanceService>();
+        services.AddTransient<RetreatService>();
+
+        var provider = services.BuildServiceProvider();
+
+        #region 反射获取所有带有CustomAttribute特性的类并调用对应方法
+
+        //反射获取所有带有CustomAttribute特性的类
+        var classes = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(type => type.GetCustomAttributes<CustomAttribute>().Any());
+
+        foreach (var clazz in classes)
+        {
+            //获取标记CustomAttribute的实例
+            var attr = clazz.GetCustomAttributes<CustomAttribute>().First();
+
+            //根据CustomAttribute元数据信息调用对应的方法
+            var methodInfo = clazz.GetMethod(attr.TargetMethod);
+            if (methodInfo != null)
+            {
+                //instance 对象是通过依赖注入容器获取的。这是一种常用的实现方式，可以使用依赖注入解耦程序中各个组件之间的依赖关系，方便测试和维护。
+                var instance = provider.GetService(clazz);
+                methodInfo.Invoke(instance, null);
+            }
+        }
+
+        #endregion
+
+        #region 反射获取所有带有CustomAttribute特性的类并调用指定方法
+
+        var executionMethod = "RetreatWay";
+
+        foreach (var clazz in classes)
+        {
+            //获取标记CustomAttribute的实例
+            var attr = clazz.GetCustomAttributes<CustomAttribute>().First();
+
+            if (attr.TargetMethod == executionMethod)
+            {
+                //根据CustomAttribute元数据信息调用对应的方法
+                var methodInfo = clazz.GetMethod(attr.TargetMethod);
+                if (methodInfo != null)
+                {
+                    //instance 对象是通过依赖注入容器获取的。这是一种常用的实现方式，可以使用依赖注入解耦程序中各个组件之间的依赖关系，方便测试和维护。
+                    var instance = provider.GetService(clazz);
+                    methodInfo.Invoke(instance, null);
+                }
+            }
+        }
+
+        #endregion
+
+        Console.ReadLine();
+    }
+}
---
+++ 工作&学习.Net训练营/Practice_Web/Controllers/DependencyInjection.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PracticeSiteWeb.Controllers
+{
+    /// <summary>
+    /// 首先，定义一个 Attribute 类型，作为需要注入的元数据：
+    /// </summary>
+    public class MyAttribute : Attribute
+    {
+        public string TargetMethod { get; set; }
+
+        public MyAttribute(string targetMethod)
+        {
+            TargetMethod = targetMethod;
+        }
+    }
+
+
+    public class DependencyInjection : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 接着，我们定义一个需要被注入的依赖：
+        /// </summary>
+        [My("Foo")]
+        public class FooService
+        {
+            public void Foo()
+            {
+                Console.WriteLine("Hello, Foo!");
+            }
+        }
+
+        [My("Bar")]
+        public class BarService
+        {
+            public void Bar()
+            {
+                Console.WriteLine("Hello, Bar!");
+            }
+        }
+
+
+
+    }
+}
