50a4058031437fe28e666c08c08b23b88aaf69cf
Edwin <1070342164@qq.com>
2023-07-09 17:17:08 +0800

    update

---
+++ 工作&学习.Net训练营/PracticeConsole/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole
+{
+    public class MyAsyncClass : Component
+    {
+        /// <summary>
+        /// 声明一个委托类型，用于定义异步操作的方法签名
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        public delegate int MyAsyncDelegate(int arg);
+
+        /// <summary>
+        /// 声明一个事件，用于通知异步操作的完成
+        /// </summary>
+        public event MyAsyncDelegate OperationNameCompleted;
+
+        /// <summary>
+        /// 异步执行方法，接受一个参数 arg
+        /// </summary>
+        /// <param name="arg"></param>
+        public void DoWorkAsync(int arg)
+        {
+            // 将异步操作放入线程池中执行
+            ThreadPool.QueueUserWorkItem(new WaitCallback(DoWork), arg);
+        }
+
+        /// <summary>
+        /// 真正的异步操作
+        /// </summary>
+        /// <param name="obj"></param>
+        private void DoWork(object obj)
+        {
+            int arg = (int)obj;
+            int res = arg + 1;
+
+            // 触发事件，传递异步操作的结果
+            OperationNameCompleted?.Invoke(res);
+        }
+    }
+
+}
---
+++ 工作&学习.Net训练营/PracticeConsole/Common/AsynchronousMethods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole.Common
+{
+    public class AsynchronousMethods
+    {
+        public async void Test()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Test Task 1 completed.");
+            });
+
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Test Task 2 completed.");
+            });
+
+            // 等待所有任务完成
+            Task.WaitAll();
+        }
+
+        public async Task TestDoSomeAsync()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("Async method completed.");
+        }
+
+        /// <summary>
+        /// 文件下载
+        /// </summary>
+        /// <param name="url">下载地址</param>
+        public void DownloadFile(string url)
+        {
+            // 创建一个WebClient实例
+            WebClient client = new WebClient();
+
+            // 启用异步下载文件操作
+            client.DownloadFileAsync(new Uri(url), "file.txt");
+
+            // 注册DownloadFileCompleted事件处理程序【定义异步操作完成后的回调函数】
+            client.DownloadFileCompleted += (sender, e) =>
+            {
+                if (e.Error != null)
+                {
+                    Console.WriteLine("文件下载出现错误：{0}", e.Error.Message);
+                }
+                else
+                {
+                    Console.WriteLine("文件下载完成！");
+                }
+            };
+
+            Console.WriteLine("按任意键退出程序");
+            Console.ReadKey();
+        }
+
+        public static async Task ThrowExceptionAsync()
+        {
+            await Task.Delay(1000);
+            throw new InvalidOperationException("Something went wrong");
+        }
+
+        public static async void HandleButtonClickAsync()
+        {
+            try
+            {
+                await ThrowExceptionAsync().ConfigureAwait(false);
+                Console.WriteLine("This line will not be executed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
