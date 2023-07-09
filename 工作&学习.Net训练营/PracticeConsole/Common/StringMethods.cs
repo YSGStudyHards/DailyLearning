a13188a328a8ec79585532c7f68ef63690303dd4
Edwin <1070342164@qq.com>
2023-07-09 20:33:27 +0800

    Add StringMethods

---
+++ 工作&学习.Net训练营/PracticeConsole/Common/StringMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole.Common
+{
+    public class StringMethods
+    {
+        public static void TestMain()
+        {
+            string str1 = null;
+            string str2 = "";
+            string str3 = " ";
+            string str4 = "追逐时光者";
+
+            Console.WriteLine(IsStringNullOrWhiteSpace(str1));// 输出：True
+            Console.WriteLine(IsStringNullOrWhiteSpace(str2));// 输出：True
+            Console.WriteLine(IsStringNullOrWhiteSpace(str3));// 输出：True
+            Console.WriteLine(IsStringNullOrWhiteSpace(str4));// 输出：False
+
+            Console.WriteLine(IsStringNullOrEmpty(str1));// 输出：True
+            Console.WriteLine(IsStringNullOrEmpty(str2));// 输出：True
+            Console.WriteLine(IsStringNullOrEmpty(str3));// 输出：False
+            Console.WriteLine(IsStringNullOrEmpty(str4));// 输出：False
+        }
+
+        public static bool IsStringNullOrEmpty(string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+
+        public static bool IsStringNullOrWhiteSpace(string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
--- 工作&学习.Net训练营/PracticeConsole/Program.cs
+++ 工作&学习.Net训练营/PracticeConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Reflection;
 
@@ -9,6 +10,30 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        string str1 = null;
+        string str2 = "";
+        string str3 = " ";
+        string str4 = "追逐时光者";
 
+        Console.WriteLine(IsStringNullOrWhiteSpace(str1));// 输出：True
+        Console.WriteLine(IsStringNullOrWhiteSpace(str2));// 输出：True
+        Console.WriteLine(IsStringNullOrWhiteSpace(str3));// 输出：True
+        Console.WriteLine(IsStringNullOrWhiteSpace(str4));// 输出：False
+
+        Console.WriteLine(IsStringNullOrEmpty(str1));// 输出：True
+        Console.WriteLine(IsStringNullOrEmpty(str2));// 输出：True
+        Console.WriteLine(IsStringNullOrEmpty(str3));// 输出：False
+        Console.WriteLine(IsStringNullOrEmpty(str4));// 输出：False
+    }
+
+    public static bool IsStringNullOrEmpty(string str)
+    {
+        return string.IsNullOrEmpty(str);
+    }
+
+
+    public static bool IsStringNullOrWhiteSpace(string str)
+    {
+        return string.IsNullOrWhiteSpace(str);
     }
 }
