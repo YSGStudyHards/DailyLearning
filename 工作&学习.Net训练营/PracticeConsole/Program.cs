43ede22d37e5ace5c2a6854c0b87765bf3666c8d
Edwin <1070342164@qq.com>
2023-10-07 00:21:33 +0800

    SkiaSharp.QrCode 快速创建二维码

--- 工作&学习.Net训练营/PracticeConsole/Program.cs
+++ 工作&学习.Net训练营/PracticeConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using Application;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Diagnostics.CodeAnalysis;
@@ -10,7 +11,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
+        GenerateQRCodeServices.GenerateQRCode();
     }
 
 }
